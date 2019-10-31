e953ca469707b144fc692f98800ad8f7e7b9d32b
Jan <jas.kocurek@wp.pl>
2019-10-31 12:38:47 +0100

    added image_classification.py file

---
+++ image_classification.py
@@ -0,0 +1,6 @@
+'''Learning to classify images using fashion mist (Reckognizing elements of clothing).'''
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
